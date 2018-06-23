a771dde6196f40e9a855e3d0b59c410490bcf99f
Anton Semenov <allcreater@gmail.com>
2018-06-23 03:39:18 +0300

    initial commit

---
+++ asq.lua
@@ -0,0 +1,94 @@
+local asq = {
+}
+
+asq.metatable = {
+    __index = asq,
+    __call = function(self, ...)
+        return self:next(...)
+    end
+}
+
+function asq.pairs(sourceTable)
+    assert(type(sourceTable) == "table")
+    local k,v
+
+    local iterator = {
+        table = sourceTable,
+        next = function()
+            k,v = next(sourceTable, k)
+            return k,v
+        end
+    }
+
+    return setmetatable(iterator, asq.metatable)
+end
+
+function asq.keys(sourceTable)
+    assert(type(sourceTable) == "table")
+    local k,v
+
+    local iterator = {
+        table = sourceTable,
+        next = function()
+            k,v = next(sourceTable, k)
+            return k
+        end
+    }
+
+    return setmetatable(iterator, asq.metatable)
+end
+
+function asq.values(sourceTable)
+    assert(type(sourceTable) == "table")
+    local k,v
+
+    local iterator = {
+        table = sourceTable,
+        next = function()
+            k,v = next(sourceTable, k)
+            return v
+        end
+    }
+
+    return setmetatable(iterator, asq.metatable)
+end
+
+
+function asq.map(iterator, mapping)
+    assert(getmetatable(iterator) == asq.metatable)
+
+    local safeMapping = function (k, ...)
+        if k then
+            return mapping(k, ...)
+        else
+            return
+        end
+    end
+
+    local resIterator = {
+        next = function()
+            return safeMapping(iterator())
+        end
+    }
+
+    return setmetatable(resIterator, asq.metatable)
+end
+
+
+local test = {
+    vasya = 125,
+    petya = 10,
+    vika = 83,
+    oleg = 113,
+    katya = 3
+}
+
+--local a = asq.map(test, function (k,v) return k .. k, v, v * v end)
+
+print("test...")
+for k,v,vv in asq.pairs(test)
+                    :map (function (k,v) return k .. k, v, v*v end) 
+                    :map (function (k,v,vv) return k, vv, v end)
+                    do
+    print(k,v,vv)
+end
