07bc0ceb08a44ad1c4a01dd4ce9f4fdaf18cbdb2
Anton Semenov <allcreater@gmail.com>
2018-08-30 02:03:27 +0300

    added toList and toTable functions

--- asq.lua
+++ asq.lua
@@ -8,12 +8,17 @@ asq.metatable = {
     end
 }
 
+-- @brief transforms source table into sequence (iterator)
+-- @return sequence of key/value pairs. Iterator is compatible with range-based for loop
 function asq.pairs(sourceTable)
     assert(type(sourceTable) == "table")
     local k,v
 
     local iterator = {
         table = sourceTable,
+        reset = function()
+            k,v = nil, nil
+        end,
         next = function()
             k,v = next(sourceTable, k)
             return k,v
@@ -23,6 +28,8 @@ function asq.pairs(sourceTable)
     return setmetatable(iterator, asq.metatable)
 end
 
+-- @brief transforms source table into sequence (iterator)
+-- @return sequence of keys. Iterator is compatible with range-based for loop
 function asq.keys(sourceTable)
     assert(type(sourceTable) == "table")
     local k,v
@@ -38,6 +45,8 @@ function asq.keys(sourceTable)
     return setmetatable(iterator, asq.metatable)
 end
 
+-- @brief transforms source table into sequence (iterator)
+-- @return sequence of table values. Iterator is compatible with range-based for loop
 function asq.values(sourceTable)
     assert(type(sourceTable) == "table")
     local k,v
@@ -53,6 +62,9 @@ function asq.values(sourceTable)
     return setmetatable(iterator, asq.metatable)
 end
 
+-- @brief transforms initial sequence into another. 
+-- @param mapping - function (v1, v2, ..., vn) -> v1`, v2`, ..., vm` 
+-- @return sequence
 
 function asq.map(iterator, mapping)
     assert(getmetatable(iterator) == asq.metatable)
@@ -74,6 +86,9 @@ function asq.map(iterator, mapping)
     return setmetatable(resIterator, asq.metatable)
 end
 
+-- @brief sorts entire sequence by user criteria (lesser to bigger)
+-- @param sortCriteria - function (...) -> number . Result number is a priority in result table
+-- @return sorted sequence
 function asq.sort(iterator, sortCriteria)
     assert(getmetatable(iterator) == asq.metatable)
 
@@ -117,7 +132,11 @@ function asq.sort(iterator, sortCriteria)
     return setmetatable(resIterator, asq.metatable)
 end
 
--- even if iterator returns tuples only first argument will be folded.
+-- @brief folds whole sequence into a single value
+-- @param iterator - input sequence. Even if returns more than one value only first will be folded
+-- @param func - function op(accumulator, newValue) must return new accumulator value
+-- @param accum - initial accumulator value 
+-- @return final accumulator value
 function asq.fold (iterator, func, accum)
     assert(getmetatable(iterator) == asq.metatable)
     assert(func and type(func) == "function")
@@ -136,6 +155,9 @@ function asq.fold (iterator, func, accum)
     return accum
 end
 
+-- @brief filters the sequence leaving only values satisfying the predicate
+-- @param predicate - function (...) -> bool
+-- @return filtered sequence (all elements where predicate was true)
 function asq.where (iterator, predicate)
     assert(getmetatable(iterator) == asq.metatable)
     assert(predicate and type(predicate) == "function")
@@ -159,6 +181,8 @@ function asq.where (iterator, predicate)
     return setmetatable(resIterator, asq.metatable)
 end
 
+-- @brief finds first element satisfying the condition
+-- @return v1,v2,...,vn
 function asq.first (iterator, predicate)
     local results
     repeat
@@ -170,6 +194,42 @@ function asq.first (iterator, predicate)
     until predicate(table.unpack(results))
 
     return table.unpack(results)
+end
+
+-- @brief gathers all sequence values into list table
+-- @param iterator
+-- @return {1 = {...}, 2 = {...}, ..., n = {...}}
+function asq.toList(iterator)
+    local list = {}
+    while true do
+        local result = {iterator()}
+        if result[1] then
+            table.insert( list, result)
+        else
+            break
+        end
+    end
+
+    return list
+end
+
+-- @brief gathers all sequence values into table
+-- @param iterator - should returns key/values pairs to map on generic Lua table 
+-- @return {k1 = v1, k2 = v2, ..., kn = vn}
+function asq.toTable(iterator)
+    local resTable = {}
+
+    while true do
+        local key, value = iterator()
+
+        if key then
+            resTable[key] = value
+        else
+            break
+        end
+    end
+
+    return resTable
 end
 
 return asq
